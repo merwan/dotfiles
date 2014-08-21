58a4821b3dea26783dc9af721e5a0a48dd9f79e3
Merouane Atig <merouane.atig@gmail.com>
2014-08-21 14:36:52 +0200

    add zshrc in setup

--- setup.sh
+++ setup.sh
@@ -10,4 +10,6 @@ ln -sb dotfiles/.vimrc .
 ln -sf dotfiles/.vim .
 ln -sf dotfiles/.githelpers .
 ln -sf dotfiles/.gitconfig .
-
+ln -sf dotfiles/.oh-my-zsh .
+ln -sf dotfiles/.zshrc .
+ln -sf dotfiles/.dir_colors .
